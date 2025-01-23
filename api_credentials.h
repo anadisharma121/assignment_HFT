363ea780013c56350c6f5490341d571d80a7affe
Anadi Sharma <sharma.anadi007@gmail.com>
2025-01-24 03:11:05 +0530

    new

---
+++ api_credentials.h
@@ -0,0 +1,11 @@
+#ifndef API_CREDENTIALS_H
+#define API_CREDENTIALS_H
+
+#include <cstdlib>  // For std::getenv
+#include <string>
+
+const std::string CLIENT_ID = "k0BFdGk3";
+const std::string CLIENT_SECRET = "Rdc_MhqI8kC5fGPRQeYdcNDGUihg3wBUhVHnU458ktQ";
+
+#endif // API_CREDENTIALS_H
+
